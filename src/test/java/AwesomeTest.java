b0ef03d0f4a2df61f4d96b69ceea76f5067a96af
Selma-Atabas <selma-aykal43@hotmail.com>
2020-10-13 22:58:17 +0200

    Added more awesome stuff

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -4,5 +4,11 @@ public class AwesomeTest {
                 "green means it is under staging area" +
                 "grey in .idea folder means it is ignored" +
                 "when the color is normal, it means it is committed no changes ");
+
+        //I just pulled the new change  from github that i did not have locally
+        //and now my project is in sync with remote repository
+
     }
+
+
 }
