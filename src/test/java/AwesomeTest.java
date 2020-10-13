a9f26b6a6e22c17a108b0355c7362ea5dbe6c085
Selma-Atabas <selma-aykal43@hotmail.com>
2020-10-13 20:31:28 +0200

    Added new file for awesme feature

---
+++ src/test/java/AwesomeTest.java
@@ -0,0 +1,8 @@
+public class AwesomeTest {
+    public static void main(String[] args) {
+        System.out.println("red mean it is untracked" +
+                "green means it is under staging area" +
+                "grey in .idea folder means it is ignored" +
+                "when the color is normal, it means it is committed no changes ");
+    }
+}
